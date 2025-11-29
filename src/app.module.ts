8233050b670f4cc243122eefdbaee15998039d06
kurovu146 <kurobakaito146@gmail.com>
2025-11-29 18:55:30 +0700

    add auth jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,18 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { BoxModule } from './box/box.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ['.env'],
+    }),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '7d' },
     }),
     PrismaModule,
     AuthModule,
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(username: string, password: string) {
+    const user = await this.prisma.user.findFirst({
+      where: { username },
+    });
+
+    if (user?.password !== password) {
+      throw new UnauthorizedException();
+    }
+
+    const payload = { id: user.id, username: user.username, role: user.role };
+    const token = await this.jwtService.signAsync(payload);
+
+    return {
+      code: 200,
+      message: 'Đăng nhập thành công!',
+      user,
+      token,
+    };
+  }
+}
