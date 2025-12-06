b937fa1674679085f9020a17b42be75be84a7aef
kurovu146 <kurobakaito146@gmail.com>
2025-12-06 14:44:57 +0700

    feat: Implement Bills Module
    This commit introduces the Bills module with basic functionalities:
    
    - Added `BillsService` for creating, retrieving, and updating bills.
    - Added `BillsController` for handling bill-related requests.
    - Updated `prisma/schema.prisma` to include new `DayType` enum and update `dayType` field to use it.
    - Added a log entry to the audit log.

--- src/bills/bills.service.ts
+++ src/bills/bills.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { BillStatus } from 'generated/prisma/enums';
+import { Bill } from 'generated/prisma/browser';
+import { Prisma } from 'generated/prisma/client';
 import { AppError } from 'libs/error/base.error';
 import { AppLogger } from 'libs/log/logger';
 import { PrismaService } from 'prisma/prisma.service';
@@ -9,37 +10,75 @@ export class BillsService {
   private readonly logger = new AppLogger(BillsService.name);
   constructor(private readonly prisma: PrismaService) {}
 
+  /**
+   * @param data - data to create a new bill
+   * @returns the created bill
+   */
   async createBill(data: {
     boxId: number;
     creatorId: number;
     subtotal: number;
     discountAmount: number;
     total: number;
-  }) {
-    const bill = await this.prisma.bill.create({
-      data: {
-        start: new Date(),
-        boxId: data.boxId,
-        creatorId: data.creatorId,
-        subtotal: data.subtotal,
-        discountAmount: data.discountAmount,
-        total: data.total,
+  }): Promise<Bill> {
+    try {
+      const bill = await this.prisma.bill.create({
+        data: {
+          start: new Date(),
+          boxId: data.boxId,
+          creatorId: data.creatorId,
+          subtotal: data.subtotal,
+          discountAmount: data.discountAmount,
+          total: data.total,
+        },
+      });
+
+      return bill;
+    } catch (error) {
+      this.logger.error(error || 'Lỗi tạo bill');
+      throw new AppError('Tạo bill thất bại', 500);
+    }
+  }
+
+  async getAllBills(): Promise<Bill[]> {
+    return this.prisma.bill.findMany();
+  }
+
+  /**
+   * @param id - id bill cần lấy
+   * @returns bill nếu tìm thấy
+   */
+  async getBill(id: number): Promise<Bill> {
+    const bill = await this.prisma.bill.findUnique({
+      where: {
+        id,
       },
     });
+
+    if (!bill) {
+      this.logger.error(`Không tìm thấy bill với id: ${id}`);
+      throw new AppError('Không tìm thấy bill', 404);
+    }
 
     return bill;
   }
 
-  async getBill(boxId: number) {
-    return this.prisma.bill.findFirst({
-      where: {
-        status: BillStatus.DRAFT,
-        boxId: boxId,
-      },
-    });
-  }
+  /**
+   * @param id - id bill cần cập nhật
+   * @param data - data (có thể là một phần của BillUpdateInput hoặc tat cả)
+   * @returns bill đã được cập nhật
+   */
+  async updateBill(id: number, data: Prisma.BillUpdateInput): Promise<Bill> {
+    try {
+      await this.getBill(id);
 
-  async updateBill(data) {
-    return this.prisma.bill.update(data);
+      return this.prisma.bill.update({
+        where: { id },
+        data,
+      });
+    } catch (error) {
+      this.logger.error(error || 'Lỗi cập nhật bill');
+      throw new AppError('Cập nhật bill thất bại', 500);
+    }
   }
 }
